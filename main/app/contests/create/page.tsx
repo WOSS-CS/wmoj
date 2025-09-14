048796b0cd1afaddc183560f8f2f435ad8eafb5d
MoizDev <1hashmimoi@hdsb.ca>
2025-09-14 11:06:42 -0400

    THE MOSSAD INTERFERED WITH DEVELO0PMENT.  Clean restart

--- main/app/contests/create/page.tsx
+++
@@ -1,5 +0,0 @@
-import { ContestCreator } from "@/components/contests/contest-creator"
-
-export default function CreateContestPage() {
-  return <ContestCreator />
-}
--- main/app/problems/create/page.tsx
+++
@@ -1,5 +0,0 @@
-import { ProblemCreator } from "@/components/problems/problem-creator"
-
-export default function CreateProblemPage() {
-  return <ProblemCreator />
-}
